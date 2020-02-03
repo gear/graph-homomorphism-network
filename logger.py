0f22ecb85715b1d3ed471a49b357ab8d0b8c5d2a
Hoang NT <hoangnt.titech@gmail.com>
2020-02-03 16:56:35 +0900

    log training

---
+++ logger.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+import time
+
+class Logger(object):
+    """Util class to log and write experiment results."""
+    def __init__(self, args, path="./log"):
+        self.args = args.__dict__
+        self.id = str(time.time()).split('.')[0]
+        self.log_file = os.path.join(path, args.dataset) 
+        os.makedirs(self.log_file, exist_ok=True)
+        self.log_file = os.path.join(self.log_file, self.id+".csv")
+        with open(self.log_file, "w") as f:
+            for k, v in self.args.items():
+                f.write("# {}: {}".format(k, v))
+
+    def write_log(self, log_dict, fold_id): 
+        fold_data = [fold_id] * len(log_dict["epoch"])
+        log_dict["fold"] = fold_data
+        data = pd.DataFrame.from_dict(log_dict)
+        header = True if fold_id == 0 else False
+        data.to_csv(self.log_file, index=False, mode="a", header=header)
+        
