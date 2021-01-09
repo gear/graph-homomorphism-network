b7c0ed05decd4da36e68d4cf13034d49aeac0c24
Hoang NT <hoangnt.titech@gmail.com>
2021-01-09 18:26:47 +0900

    Update test_hom_utils.py

--- tests/test_hom_utils.py
+++ tests/test_hom_utils.py
@@ -53,21 +53,21 @@ def test_add_two_loops(g):
 
 
 def test_cycle_list():
-    cyclel2 = path_list(size=4, to_homlib=True, num_loops=1)
+    cyclel2 = path_list(size=4, num_loops=1)
     assert 12 == len(cyclel2)
 
 
 def test_path_list():
-    pathl2 = path_list(size=2, to_homlib=True, num_loops=0)
+    pathl2 = path_list(size=2, num_loops=0)
     assert 1 == len(pathl2)
 
 
 def test_tree_list():
-    treel2 = tree_list(size=2, to_homlib=True, num_loops=0)
+    treel2 = tree_list(size=2, num_loops=0)
     assert 1 == len(treel2)
-    treel2 = tree_list(size=2, to_homlib=True, num_loops=1)
+    treel2 = tree_list(size=2, num_loops=1)
     assert 3 == len(treel2)
-    treel6 = tree_list(size=6, to_homlib=False, num_loops=0)
+    treel6 = tree_list(size=6, num_loops=0)
     assert 13 == len(treel6)
-    treel6 = tree_list(size=6, to_homlib=False, num_loops=1)
+    treel6 = tree_list(size=6, num_loops=1)
     assert 77 == len(treel6)
