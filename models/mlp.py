83695119ce0ef0eea949b5b63e7d53009dc26766
Hoang NT <me@gearons.org>
2021-01-14 23:40:17 +0900

    verbose

--- models/mlp.py
+++ models/mlp.py
@@ -9,6 +9,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 from torch.utils.data import TensorDataset, DataLoader
+import numpy as np
 from tqdm import tqdm
 from ghc.homomorphism import get_hom_profile
 from ghc.utils.data import load_data, load_precompute,\
@@ -62,6 +63,7 @@ if __name__ == "__main__":
     parser.add_argument('--dropout', type=float, default=0.5)
     parser.add_argument('--patience', type=int, default=500)
     parser.add_argument('--cuda', action="store_true", default=False)
+    parser.add_argument('--verbose', action="store_true", default=False)
     parser.add_argument('--gpu_id', type=int, default=0)
     parser.add_argument("--log_period", type=int, default=200)
     args = parser.parse_args()
@@ -112,7 +114,8 @@ if __name__ == "__main__":
             acc_test = accuracy(output[idx], tensory[idx])
             return loss_test.item(), acc_test.item()
     #### Run for 10-folds scores
-    for split in splits:
+    scores = []
+    for split in tqdm(splits, desc="10-folds"):
         model = MLP(tensorX.size(-1), int(tensory.max()+1), args.hids,
                     dp=args.dropout).to(device)
         opt_config = [{'params': model.parameters(),
@@ -124,17 +127,19 @@ if __name__ == "__main__":
         idx_test = torch.Tensor(idx_test).long().to(device)
 
         checkpt_file = 'checkpoints/'+uuid.uuid4().hex[:4]+'-'+args.data+'.pt'
-        print(device_id, checkpt_file)
+        if args.verbose:
+            print(device_id, checkpt_file)
         c = 0
         best = 0
         best_epoch = 0
         acc = 0
         for epoch in range(args.epochs):
             loss_train, acc_train = train(model, optimizer, idx_train)
-            if (epoch+1)%args.log_period == 0 or epoch == 0:
-                print('Epoch:{:04d}'.format(epoch+1),
-                    'loss:{:.3f}'.format(loss_train),
-                    'acc:{:.2f}'.format(acc_train*100))
+            if args.verbose:
+                if (epoch+1)%args.log_period == 0 or epoch == 0:
+                    print('Epoch:{:04d}'.format(epoch+1),
+                        'loss:{:.3f}'.format(loss_train),
+                        'acc:{:.2f}'.format(acc_train*100))
             if acc_train > best:
                 best = acc_train
                 best_epoch = epoch
@@ -145,4 +150,6 @@ if __name__ == "__main__":
             if c == args.patience:
                 break
         _, test_acc = test(model, idx_test, checkpt_file)
-        print('Load {}th epoch. Test acc:{:.4f}'.format(best_epoch, test_acc))
+        scores.append(test_acc)
+    scores = np.array(scores)
+    print('CV score:{:.4f}, {:.4f}'.format(scores.mean(), scores.std()))
